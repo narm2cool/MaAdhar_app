9736dc8da15a4cdf8b93dc440db4105cd128f45c
narm2cool <narmadhamayilraj@gmail.com>
2022-10-20 16:47:37 +0530

    aadahar app

---
+++ testing automation/src/com/example/testng/Delete.java
@@ -0,0 +1,31 @@
+package com.example.testng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Delete {
+	
+	WebDriver webdriver = null;
+	String URL = "http://localhost:4200/";
+
+	@Test
+	public void delete() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hp-p\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver webdriver = new ChromeDriver();
+		webdriver.get(URL);
+		webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		webdriver.manage().window().maximize();
+		webdriver.findElement(By.xpath("//button[normalize-space()='Admin LogIn']")).click();
+		webdriver.findElement(By.xpath("//input[@id='username']")).sendKeys("Admin");
+		webdriver.findElement(By.xpath("//input[@id='password']")).sendKeys("1234");
+		webdriver.findElement(By.xpath("//button[normalize-space()='Submit']")).click();
+		webdriver.findElement(By.xpath("//body[1]/app-root[1]/div[1]/div[2]/div[1]/app-myaadhar[1]/div[1]/div[1]/div[8]/div[1]/div[2]/div[1]/a[2]/i[1]")).click();
+		webdriver.findElement(By.xpath("//button[normalize-space()='Yes']")).click();
+		System.out.println("Deleted Successfully");
+	}
+}
